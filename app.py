4b1fd4949718717ec69607208a9fd67b593e1137
Ritvik Tabishala <scholarritvik@gmail.com>
2026-02-04 04:19:26 +0530

    initial backend final commit

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask, render_template, request, redirect, url_for
 import random
 import string
 
 app = Flask(__name__)
 
-url_db  = {} #temporary storage
+url_db = {}  # temporary storage
+
 
 def generate(length=6):
     return "".join(random.choices(string.ascii_letters + string.digits, k=length))
 
-@app.route("/",methods=["GET","POST"])
-def home():
-    short_url = None
 
-    if request.method=="POST":
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
         original_url = request.form["url"]
         code = generate()
         url_db[code] = original_url
-        short_url = request.host_url + code
-    
-    return render_template("index.html",short_url=short_url)
+        return redirect(url_for("result", code=code))
+
+    return render_template("index.html")
+
+
+@app.route("/result/<code>")
+def result(code):
+    short_url = url_for("redirect_url", code=code, _external=True)
+    return render_template("index.html", short_url=short_url)
+
 
 @app.route("/<code>")
 def redirect_url(code):
@@ -27,6 +34,7 @@ def redirect_url(code):
     if original_url:
         return redirect(original_url)
     return "URL not found!"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
